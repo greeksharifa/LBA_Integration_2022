e15b0e0bb819d67c2902a9b3680432fa6f32e1ea
greeksharifa <greeksharifa@gmail.com>
2024-01-22 18:39:11 +0900

    fix to make KAIST output

--- 2nd_year/AnotherMissOh_test.py
+++ 2nd_year/AnotherMissOh_test.py
@@ -31,16 +31,13 @@ def process_raw_sentence(exception_object=[], sentences=['This is an example.'],
 	new_data=[]
 	if not generate_unanswerable_que:
 		for question in sentences:
-			# question = sentence
-
 			extracted_objects_ = []
-			
-
 			word_tokenized_question = word_tokenize(question)
 			try:
 				extracted_objects = utils.object_extract(question)
 			except:
-				ipdb.set_trace()
+				# ipdb.set_trace()
+				print("extraction error occured.")
 
 			for object_, [object_idx1, object_idx2] in extracted_objects:
 			# for object_item in extracted_objects:
@@ -48,8 +45,8 @@ def process_raw_sentence(exception_object=[], sentences=['This is an example.'],
 				if not sum(exception_list) > 0:
 					extracted_objects_.append([[object_], [object_idx1, object_idx2]])
 
-			if len(extracted_objects_) == 0:
-				ipdb.set_trace()
+			# if len(extracted_objects_) == 0:
+				# ipdb.set_trace()
 			new_data.append([word_tokenized_question, extracted_objects_])
 		return new_data
 		# if not generate_unanswerable_que:
@@ -118,14 +115,15 @@ def inference_demo(model, classifier_head, inputs):
 
 def main(args):
 	model, tokenizer = utils.get_model(args)
-	model.load_state_dict(torch.load('/mnt/hsyoon/workspace/LBA-ARVQA/saves/ckpt/2023-10-30_15:13:30_best_loss.pt'))
+	model.load_state_dict(torch.load(args.model_ckpt))
 	model.to(device)
 
 	classifier_head = utils.Predicate2Bool(model.config.hidden_size)
-	classifier_head.load_state_dict(torch.load('/mnt/hsyoon/workspace/LBA-ARVQA/saves/ckpt/2023-10-30_15:13:30_classifier_best_loss.pt'))
+	classifier_head.load_state_dict(torch.load(args.classifier_head_ckpt))
 	classifier_head.to(device)
 
-	test_questions = utils_sys.read_json(os.path.join(args.dataset_dir, 'DramaQA/AnotherMissOhQA_test_set.json'))
+	json_data = utils_sys.read_json(os.path.join(args.dataset_dir, f'DramaQA/AnotherMissOhQA_{args.split}_set.json' ))
+	test_questions = json_data
 	sg_fpath = os.path.join(args.dataset_dir, 'AnotherMissOh', 'scene_graph')
 
 	exception_object = ['Haeyoung', 'Jinsang', 'Taejin', 'relationship', 'kind', 'something', 'communication', 'someone', 'everything', 'com', 'color']
@@ -133,67 +131,123 @@ def main(args):
 
 	# for debug, we are currently input scene graph from AnotherMissOh. Maybe the caption of the video can be inputed instead
 	test_sg2sentence = {}
-	for question in tqdm(test_questions[0:1]):
+	for question in tqdm(test_questions):
 		test_sg = utils.AnotherMissOh_sg(args, sg_fpath, question)
 		test_sg2sentence[question['qid']] = ". ".join(test_sg)
 
 	# tokenize the sentenced scene graph
-	tokenized_sg2sentence = utils.sentence2tokenize(args, tokenizer, test_sg2sentence)
+	tokenized_sg2sentence = utils.sentence2tokenize(args, tokenizer, test_sg2sentence) # TODO: scene graph를 문장화 하여 tokenize 한 결과
 
 	# you can input user given sentence like "this is an exmaple"
 	# if you input generate_unanswerable_que=True, it will generate unanwerable by folloing the object list. 
 	# for debug, we utilize AnotherMissOh obejct list as a dummy
 
 	# for fast test
-	test_question = test_questions[0]['que']
-	test_qid = test_questions[0]['qid']
-
-	object_list=utils_sys.read_pkl('/mnt/hsyoon/workspace/LBA-ARVQA/saves/custom_dataset/AnotherMissOhQA_object_list.pkl')
-	data_instance = process_raw_sentence(exception_object=[], sentences=[test_question], object_list=None, generate_unanswerable_que=False) 
-
-	# For the case of using user given input question
-	# data_instance = process_raw_sentence(exception_object=[], sentences=['This is an example.', 'I have a cat'], object_list=None, generate_unanswerable_que=False) 
-
-
-	# For the case when generating unanswerable question at the same time with AnotherMissOh
-	# data_instance = process_raw_sentence(exception_object=exception_object, sentences=['This is an example.'], object_list=object_list, generate_unanswerable_que=True) 
-	inputs = utils.input_preprocess_demo(args, tokenizer, data_instance, tokenized_sg2sentence[test_qid])
-	with torch.no_grad():
-		for idx, item in enumerate(inputs):
-			# label = [[batch[-2], batch[-1].bool()] for batch in item ]
-			prediction = inference_demo(model, classifier_head, item)
-			answerability = []
-			for idx, pred in prediction:
-				if False in pred.values():
-					answerability.append('unanwerable')
-				else:
-					answerability.append('anwerable')
-
-			ipdb.set_trace()
-			
+	outputs = []
+	count_unanswerable = 0
 	return
+	for i in range(len(test_questions)):
+		test_question = test_questions[i]['que'] # TODO: 디버깅을 위해 첫번째 question만 가져오도록 설정
+		test_qid = test_questions[i]['qid']
+		test_vid = test_questions[i]['vid']
+
+		object_list=utils_sys.read_pkl(os.path.join(args.custom_dataset_dir, args.object_list_path))
+		data_instance = process_raw_sentence(exception_object=[], sentences=[test_question], object_list=None, generate_unanswerable_que=False)
+		print('data_instance:', data_instance)
+		# TODO: answerability를 확인하고자 하는 질문
+		# 		- generate_unanswerable_que 를 True로 주면 object_list 에서 주어지는 object 들로 unanswerable data를 생성하여 데이터를 구성함.
+		#     	- generate_unanswerable_que 를 False로 주면 unanswerable data 생성 없이 주어진 입력으로만 answerability 판단
+		#     	- 한 문장 내에 False가 하나라도 있다면 대답 불가능한 질문으로 간주하게 됩니다.
+		# 		- sentences: 입력 비디오에 대해 answerability를 확인하고자 하는 질문들 (list of string)
+
+		# For the case of using user given input question
+		# data_instance = process_raw_sentence(exception_object=[], sentences=['This is an example.', 'I have a cat'], object_list=None, generate_unanswerable_que=False) 
+
+
+		# For the case when generating unanswerable question at the same time with AnotherMissOh
+		# data_instance = process_raw_sentence(exception_object=exception_object, sentences=['This is an example.'], object_list=object_list, generate_unanswerable_que=True) 
+		try:
+			inputs = utils.input_preprocess_demo(args, tokenizer, data_instance, tokenized_sg2sentence[test_qid])
+			
+			with torch.no_grad():
+				if len(inputs) > 1:
+					ipdb.set_trace()
+					
+				for idx, item in enumerate(inputs):
+					# label = [[batch[-2], batch[-1].bool()] for batch in item ]
+					prediction = inference_demo(model, classifier_head, item)
+					answerability = []
+					for idx, pred in enumerate(prediction):
+						if False in pred.values():
+							answerability.append('unanswerable')
+							count_unanswerable += 1
+						else:
+							answerability.append('answerable')
+					
+					print('prediction:', prediction)
+					print('answerability:', answerability)
+					
+					output = {
+						'qid': test_qid,
+						'question': test_question,
+						'answerability': answerability,
+						'prediction': prediction,
+						'vid': test_vid,
+					}
+					outputs.append(output)
+
+			# ipdb.set_trace()
+		except:
+			print('preprocess_demo error')
+			output = {
+						'qid': test_qid,
+						'question': test_question,
+						'answerability': ['answerable'],
+						'prediction': [],
+						'vid': test_vid,
+					}
+			outputs.append(output)
+		
+	print('outputs:', outputs)
+	json.dump(outputs, open(args.output_path, 'w'), indent=4)
+	print('count_unanswerable:', count_unanswerable)
+	
+	return
+
+
 if __name__ =="__main__":
 	parser = argparse.ArgumentParser(description='LBAagent-project')
 	#parser.add_argument('--v_sg_path', type=str, default='/mnt/hdd/hsyoon/workspace/OOD/VQRR/gqa_data/train_sceneGraphs.json')
-	parser.add_argument('--sg_info', type=str, default='/mnt/hsyoon/workspace/LBA-ARVQA/dataset/AnotherMissOh/scene_graph/AnotherMissOh01/001/0078/custom_data_info.json')
+	# parser.add_argument('--sg_info', type=str, default='./dataset/AnotherMissOh/scene_graph/AnotherMissOh01/001/0078/custom_data_info.json')
 
-	parser.add_argument('--train_scenegraph', type=str, default='/mnt/hsyoon/workspace/LBA-ARVQA/dataset/AnotherMissOh/scene_graph/AnotherMissOh15/001/0025/custom_prediction.json')
+	# parser.add_argument('--train_scenegraph', type=str, default='./dataset/AnotherMissOh/scene_graph/AnotherMissOh15/001/0025/custom_prediction.json')
 	# parser.add_argument('--val_scenegraph', type=str, default='/mnt/hdd/hsyoon/workspace/OOD/VQRR/gqa_data/val_sceneGraphs.json')
 
-	parser.add_argument('--train_question', type=str, default='/mnt/hsyoon/workspace/LBA-ARVQA/dataset/DramaQA/AnotherMissOhQA_test_set.json')
-	parser.add_argument('--dummy_question', type=str, default='/mnt/hsyoon/workspace/LBA-ARVQA/dataset/processed/train_data_fixed.pkl')
+	# parser.add_argument('--train_question', type=str, default='./dataset/DramaQA/AnotherMissOhQA_test_set.json')
+	# parser.add_argument('--train_question', type=str, default='./dataset/DramaQA/AnotherMissOhQA_val_set.json')
+	# parser.add_argument('--dummy_question', type=str, default='./dataset/processed/train_data_fixed.pkl')
 	# parser.add_argument('--val_question', type=str, default='/mnt/hdd/hsyoon/workspace/OOD/VQRR/val_data_fixed.pkl')
-
+	
+	parser.add_argument('--split', type=str, choices=['train', 'val', 'test'], default='val')
+	
+	# model
 	parser.add_argument('--bbox_threshold', type=float, default=0.3)
 	parser.add_argument('--rel_threshold', type=float, default=0.7)
-
 	parser.add_argument('--model_name', type=str, default='bert-base-uncased', choices=['bert-base-uncased'])
 	parser.add_argument('--max_length', type=int, default=512)
 	parser.add_argument('--bsz', type=int, default=4) 
 	parser.add_argument('--sg_rels_topk', type=int, default=50)
-
+	
+	# ckpt path
+	parser.add_argument('--model_ckpt', type=str, default='./saves/ckpt/2023-11-04_04_24_48_best_loss.pt')
+	parser.add_argument('--classifier_head_ckpt', type=str, default='./saves/ckpt/2023-11-04_04_24_48_classifier_best_loss.pt')
+	
+	# data path
 	parser.add_argument('--custom_dataset_dir', type=str, default='./saves/custom_dataset')
-	parser.add_argument('--dataset_dir', type=str, default='../dataset')
+	parser.add_argument('--dataset_dir', type=str, default='./dataset')
+	parser.add_argument('--object_list_path', type=str, default='AnotherMissOhQA_object_list.pkl')
+	parser.add_argument('--output_path', type=str, default='../../Integration-Outputs/output_KAIST.json')
+	
 	args = parser.parse_args()
 	
 	main(args)
